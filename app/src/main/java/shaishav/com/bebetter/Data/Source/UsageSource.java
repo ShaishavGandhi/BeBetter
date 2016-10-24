80138ac5c49d8fd4cdf9842574a41c815a8d14e4
Shaishav Gandhi <shaishgandhi@gmail.com>
2016-10-23 22:32:14 -0700

    Update Usage To Content Provider
    * Usages to content provider
    
    Signed-off-by: Shaishav Gandhi <shaishgandhi@gmail.com>

--- app/src/main/java/shaishav/com/bebetter/Data/Source/UsageSource.java
+++ app/src/main/java/shaishav/com/bebetter/Data/Source/UsageSource.java
@@ -9,8 +9,12 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
+import shaishav.com.bebetter.Data.MySQLiteHelper;
+import shaishav.com.bebetter.Data.contracts.UsageContract;
 import shaishav.com.bebetter.Data.models.Usage;
 import shaishav.com.bebetter.Data.MySQLiteHelper;
+import shaishav.com.bebetter.Data.providers.GoalProvider;
+import shaishav.com.bebetter.Data.providers.UsageProvider;
 
 /**
  * Created by Shaishav on 26-06-2016.
@@ -38,15 +42,15 @@ public class UsageSource {
 
     public Usage createUsage(long date, long usage){
         ContentValues contentValues = new ContentValues();
-        contentValues.put(MySQLiteHelper.COLUMN_DATE,date);
-        contentValues.put(MySQLiteHelper.COLUMN_USAGE, usage);
+        contentValues.put(UsageContract.COLUMN_DATE,date);
+        contentValues.put(UsageContract.COLUMN_USAGE, usage);
 
-        long insertId = database.insert(MySQLiteHelper.TABLE_USAGE, null, contentValues);
+        long insertId = database.insert(UsageContract.TABLE_USAGE, null, contentValues);
 
 
 
-        Cursor cursor = database.query(MySQLiteHelper.TABLE_USAGE,null,MySQLiteHelper.COLUMN_ID+"="+insertId,null,null,null
-                ,MySQLiteHelper.COLUMN_ID+" desc");
+        Cursor cursor = database.query(UsageContract.TABLE_USAGE,null,UsageContract.COLUMN_ID+"="+insertId,null,null,null
+                ,UsageContract.COLUMN_ID+" desc");
 
         cursor.moveToFirst();
 
@@ -62,8 +66,8 @@ public class UsageSource {
     public List<Usage> getAllUsages(){
         List<Usage> posts = new ArrayList<Usage>();
 
-        Cursor cursor = database.query(MySQLiteHelper.TABLE_USAGE,
-                null, null, null, null, null, MySQLiteHelper.COLUMN_ID+" desc");
+        Cursor cursor = database.query(UsageContract.TABLE_USAGE,
+                null, null, null, null, null, UsageContract.COLUMN_ID+" desc");
 
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
@@ -76,32 +80,34 @@ public class UsageSource {
         return posts;
     }
 
-    public List<Usage> getData(long lower_threshold,long higher_threshold){
+    public static List<Usage> getData(Context context, long lower_threshold,long higher_threshold){
 
-        List<Usage> posts = new ArrayList<Usage>();
+        List<Usage> usages = new ArrayList<Usage>();
 
-        Cursor cursor = database.query(MySQLiteHelper.TABLE_USAGE,
-                null, MySQLiteHelper.COLUMN_DATE + " > "+lower_threshold+" AND "+MySQLiteHelper.COLUMN_DATE+" < "+higher_threshold,
-                null, null, null, MySQLiteHelper.COLUMN_DATE+" ASC");
+        String[] args = new String[]{String.valueOf(lower_threshold), String.valueOf(higher_threshold)};
+
+        Cursor cursor = context.getContentResolver().query(UsageProvider.CONTENT_URI, null, UsageProvider.QUERY_SELECTION_ARGS_USAGE_RANGE,
+                args, GoalProvider.QUERY_SORT_ORDER);
+
 
         cursor.moveToFirst();
 
         while (!cursor.isAfterLast()) {
-            Usage post = cursorToPost(cursor);
-            posts.add(post);
+            Usage usage = UsageProvider.cursorToUsage(cursor);
+            usages.add(usage);
             cursor.moveToNext();
         }
         // make sure to close the cursor
         cursor.close();
-        return posts;
+        return usages;
 
     }
 
     public List<Usage> getUsagesForBackup(){
         List<Usage> posts = new ArrayList<Usage>();
 
-        Cursor cursor = database.query(MySQLiteHelper.TABLE_USAGE,
-                null, MySQLiteHelper.COLUMN_SERVER_ID+" = 'NA'", null, null, null, MySQLiteHelper.COLUMN_DATE+" asc");
+        Cursor cursor = database.query(UsageContract.TABLE_USAGE,
+                null, UsageContract.COLUMN_SERVER_ID+" = 'NA'", null, null, null, UsageContract.COLUMN_DATE+" asc");
 
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
@@ -114,11 +120,13 @@ public class UsageSource {
         return posts;
     }
 
-    public int getTotalUsage(){
-        Cursor cur = database.rawQuery("SELECT SUM("+MySQLiteHelper.COLUMN_USAGE+") FROM "+MySQLiteHelper.TABLE_USAGE, null);
-        if(cur.moveToFirst())
+    public static int getTotalUsage(Context context){
+        Cursor cursor = context.getContentResolver().query(UsageProvider.CONTENT_URI,
+                new String[]{"MAX(" + UsageContract.COLUMN_USAGE + ") "}, null, null, null);
+
+        if(cursor.moveToFirst())
         {
-            return cur.getInt(0);
+            return cursor.getInt(0);
         }
 
         return 0;
@@ -126,16 +134,16 @@ public class UsageSource {
 
     public void setServerId(String server_id,int id){
         ContentValues cv = new ContentValues();
-        cv.put(MySQLiteHelper.COLUMN_SERVER_ID,server_id);
+        cv.put(UsageContract.COLUMN_SERVER_ID,server_id);
 
-        database.update(MySQLiteHelper.TABLE_USAGE,cv,MySQLiteHelper.COLUMN_ID+" = "+id,null);
+        database.update(UsageContract.TABLE_USAGE,cv,UsageContract.COLUMN_ID+" = "+id,null);
     }
 
 
     public boolean isExisting(String server_id){
 
-        Cursor cursor = database.query(MySQLiteHelper.TABLE_USAGE,
-                null, MySQLiteHelper.COLUMN_SERVER_ID+" = '"+server_id+"'", null, null, null, MySQLiteHelper.COLUMN_DATE+" desc");
+        Cursor cursor = database.query(UsageContract.TABLE_USAGE,
+                null, UsageContract.COLUMN_SERVER_ID+" = '"+server_id+"'", null, null, null, UsageContract.COLUMN_DATE+" desc");
 
         cursor.moveToFirst();
 
@@ -150,9 +158,9 @@ public class UsageSource {
     private Usage cursorToPost(Cursor cursor){
         Usage post = new Usage();
         post.setId(cursor.getLong(0));
-        post.setDate(cursor.getLong(cursor.getColumnIndex(MySQLiteHelper.COLUMN_DATE)));
-        post.setUsage(cursor.getLong(cursor.getColumnIndex(MySQLiteHelper.COLUMN_USAGE)));
-        post.setServer_id(cursor.getString(cursor.getColumnIndex(MySQLiteHelper.COLUMN_SERVER_ID)));
+        post.setDate(cursor.getLong(cursor.getColumnIndex(UsageContract.COLUMN_DATE)));
+        post.setUsage(cursor.getLong(cursor.getColumnIndex(UsageContract.COLUMN_USAGE)));
+        post.setServer_id(cursor.getString(cursor.getColumnIndex(UsageContract.COLUMN_SERVER_ID)));
 
         return post;
     }
