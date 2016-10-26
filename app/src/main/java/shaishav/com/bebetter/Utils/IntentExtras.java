9dff80f1089e6fd83b1464f3f479b78a71ec2845
Shaishav Gandhi <shaishgandhi@gmail.com>
2016-10-25 22:22:03 -0700

    Add New Editor To Share Experience
    Signed-off-by: Shaishav Gandhi <shaishgandhi@gmail.com>

---
+++ app/src/main/java/shaishav/com/bebetter/Utils/IntentExtras.java
@@ -0,0 +1,11 @@
+package shaishav.com.bebetter.Utils;
+
+/**
+ * Created by shaishavgandhi05 on 10/25/16.
+ */
+
+public class IntentExtras {
+
+    public static final String EXPERIENCE_STRING            = "EXPERIENCE_STRING";
+
+}
