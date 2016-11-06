d680d309199f130602a9f3cf3957c89ba6e0fc85
Shaishav Gandhi <shaishgandhi@gmail.com>
2016-11-05 21:35:01 -0700

    v3 of App
    Signed-off-by: Shaishav Gandhi <shaishgandhi@gmail.com>

--- app/src/main/java/shaishav/com/bebetter/activities/Login.java
+++ app/src/main/java/shaishav/com/bebetter/activities/Login.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,6 +22,13 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.Map;
@@ -34,14 +43,17 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
-        View.OnClickListener, ApiCallback{
+        View.OnClickListener{
 
     final int RC_SIGN_IN = 0;
     PreferenceSource preferenceSource;
     GoogleApiClient mGoogleApiClient;
     ProgressDialog progressDialog;
-    String display_pic;
-    ApiResponseReceiver apiResponseReceiver;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private String TAG = Login.class.getName();
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -60,23 +72,35 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        apiResponseReceiver = new ApiResponseReceiver(getApplicationContext(), this);
+        mAuth = FirebaseAuth.getInstance();
 
-        //Initialize all variables
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        //Initialize all variable
         initialize();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .build();
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
-        if(mGoogleApiClient.isConnected()){
-            signOut();
-        }
 
         //Set event listeners
         setEventListeners();
@@ -85,7 +109,6 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
     public void initialize(){
         IntentFilter intentFilter = new IntentFilter("loginUser");
-        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(apiResponseReceiver, intentFilter);
         preferenceSource = PreferenceSource.getInstance(getApplicationContext());
         progressDialog = new ProgressDialog(Login.this);
         progressDialog.setIndeterminate(true);
@@ -121,61 +144,44 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+//      // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            progressDialog.show();
-            handleSignInResult(result);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
         }
     }
 
-    private void handleSignInResult(GoogleSignInResult result) {
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            final String name = acct.getDisplayName();
-            final String email = acct.getEmail();
-            final String gcm_token = FirebaseInstanceId.getInstance().getToken();
-            display_pic = "";
-            if(acct.getPhotoUrl() != null)
-                display_pic = acct.getPhotoUrl().toString();
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
 
-            final User user = new User();
-            user.setName(name);
-            user.setEmail(email);
-            user.setPhoto(display_pic);
-            user.setGcm_id(gcm_token);
-
-            ApiServiceLayer.loginUser(getApplicationContext(), user);
-
-        } else {
-            // Signed out, show unauthenticated UI.
-            progressDialog.hide();
-            View view = findViewById(R.id.sign_in_button);
-            Snackbar.make(view,"Couldn't sign in",Snackbar.LENGTH_SHORT).show();
-        }
-    }
-
-    private void signOut() {
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onResult(Status status) {
-                        Toast.makeText(getApplicationContext(),"Signed Out!",Toast.LENGTH_SHORT).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
-    }
-
-    @Override
-    public void onApiComplete(String action, Map<String, Object> args) {
-        progressDialog.dismiss();
-        if(action.equals("loginUser")){
-            Intent intent = new Intent(Login.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
     }
 
     @Override
@@ -188,13 +194,5 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(apiResponseReceiver != null) {
-            LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(apiResponseReceiver);
-        }
-    }
-
-    @Override
-    public void onApiError(String action, int responseCode) {
-
     }
 }
