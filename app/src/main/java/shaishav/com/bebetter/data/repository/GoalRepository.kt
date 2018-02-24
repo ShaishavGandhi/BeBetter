9ee17e2e1174b9a8c489e3486120026efc1c09bc
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-02-24 00:26:49 -0800

    Clean up
    * Add new api to get current goal
    
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- app/src/main/java/shaishav/com/bebetter/data/repository/GoalRepository.kt
+++ app/src/main/java/shaishav/com/bebetter/data/repository/GoalRepository.kt
@@ -8,9 +8,13 @@ import javax.inject.Inject
 /**
  * Created by shaishav.gandhi on 12/25/17.
  */
-class GoalRepository @Inject constructor(val databaseManager: GoalDatabaseManager) {
+class GoalRepository @Inject constructor(private val databaseManager: GoalDatabaseManager) {
 
-    fun goals(): Observable<List<Goal>> {
-        return databaseManager.goals()
-    }
+  fun goals(): Observable<List<Goal>> {
+    return databaseManager.goals()
+  }
+
+  fun currentGoal(): Observable<Goal> {
+    return databaseManager.currentGoal()
+  }
 }
