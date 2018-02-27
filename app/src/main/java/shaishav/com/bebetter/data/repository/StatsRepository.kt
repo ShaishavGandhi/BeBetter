53f6be70f5b3a5cffcc1153f4d6d1e0932e6c5a4
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-02-26 23:39:42 -0800

    Remove references to PreferenceSource
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

---
+++ app/src/main/java/shaishav/com/bebetter/data/repository/StatsRepository.kt
@@ -0,0 +1,19 @@
+package shaishav.com.bebetter.data.repository
+
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import shaishav.com.bebetter.data.models.Stat
+import javax.inject.Inject
+
+/**
+ * Created by shaishav.gandhi on 2/26/18.
+ */
+class StatsRepository @Inject constructor(private val usageRepository: UsageRepository, private val goalRepository: GoalRepository) {
+
+  fun getStat(): Observable<Stat> {
+    return Observable.combineLatest(usageRepository.dailyUsage(), goalRepository.currentGoal(), BiFunction { usage, goal ->
+      return@BiFunction Stat(usage, goal.goal)
+    })
+  }
+
+}
