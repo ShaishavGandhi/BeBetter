203f02992437411dcceb89c5eb30df5d15b8b96b
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-03-03 09:06:55 -0800

    Add goal card
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- app/src/main/java/shaishav/com/bebetter/presenter/SummaryPresenter.kt
+++ app/src/main/java/shaishav/com/bebetter/presenter/SummaryPresenter.kt
@@ -12,93 +12,111 @@ import javax.inject.Inject
  * Created by shaishav.gandhi on 12/17/17.
  */
 class SummaryPresenter @Inject constructor(
-        var view : SummaryContract,
+        var view: SummaryContract?,
         val usageRepository: UsageRepository,
         val goalRepository: GoalRepository,
         val disposables: CompositeDisposable) {
 
-    init {
-        currentSession()
-        dailyUsage()
-        averageDailyUsage()
-        totalUsage()
-        usageTrend()
-    }
+  init {
+    currentSession()
+    dailyUsage()
+    averageDailyUsage()
+    totalUsage()
+    usageTrend()
+    currentGoal()
+  }
 
 
-    fun currentSession() {
-        val disposable = usageRepository
-                .currentSession()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    view.setCurrentSession(it)
-                }
-        disposables.add(disposable)
-    }
+  fun currentSession() {
+    val disposable = usageRepository
+            .currentSession()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+              view?.setCurrentSession(it)
+            }
+    disposables.add(disposable)
+  }
 
-    fun dailyUsage() {
-        val disposable = usageRepository
-                .dailyUsage()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    view.setDailyUsage(it)
-                }
+  fun currentGoal() {
+    val disposable = goalRepository
+            .currentGoal()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { goal ->
+              view?.setCurrentGoal(goal)
+            }
 
-        disposables.add(disposable)
-    }
+    disposables.add(disposable)
+  }
 
-    fun averageDailyUsage() {
-        val disposable = usageRepository
-                .averageDailyUsage()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    view.setAverageDaiyUsage(it)
-                }
+  fun dailyUsage() {
+    val disposable = usageRepository
+            .dailyUsage()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+              view?.setDailyUsage(it)
+            }
 
-        disposables.add(disposable)
-    }
+    disposables.add(disposable)
+  }
 
-    fun totalUsage() {
-        val disposable = usageRepository
-                .totalUsage()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    view.setTotalUsage(it)
-                }
+  fun averageDailyUsage() {
+    val disposable = usageRepository
+            .averageDailyUsage()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+              view?.setAverageDaiyUsage(it)
+            }
 
-        disposables.add(disposable)
-    }
+    disposables.add(disposable)
+  }
 
-    fun usageTrend() {
-        usages()
-        goals()
-    }
+  fun totalUsage() {
+    val disposable = usageRepository
+            .totalUsage()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+              view?.setTotalUsage(it)
+            }
 
-    fun usages() {
-        val disposable = usageRepository
-                .usages()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    view.setUsages(it)
-                }
+    disposables.add(disposable)
+  }
 
-        disposables.add(disposable)
-    }
+  fun usageTrend() {
+    usages()
+    goals()
+  }
 
-    fun goals() {
-        val disposable = goalRepository
-                .goals()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    view.setGoals(it)
-                }
+  fun usages() {
+    val disposable = usageRepository
+            .usages()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+              view?.setUsages(it)
+            }
 
-        disposables.add(disposable)
-    }
+    disposables.add(disposable)
+  }
+
+  fun goals() {
+    val disposable = goalRepository
+            .goals()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+              view?.setGoals(it)
+            }
+
+    disposables.add(disposable)
+  }
+
+  fun unsubscribe() {
+    disposables.dispose()
+    view = null
+  }
 }
